af1730e5dbb90f8b18eb5605572886e33b1c1d23
Linkai Peng <72010396+lkpengcs@users.noreply.github.com>
2025-03-04 09:30:51 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,9 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://videoads.netlify.app/', 
+  base: '/', 
+  output: 'static', //
   integrations: [tailwind(), icon(), mdx(), react()],
   markdown: {
     shikiConfig: {
