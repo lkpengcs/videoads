2b12b2746494845dd8475b3dfe01f5734c618bdb
lkpengcs <11912922@mail.sustech.edu.cn>
2025-10-03 21:29:04 -0500

    update pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
-
+import remarkMath from 'remark-math'
+import rehypeMathjax from 'rehype-mathjax'
 import react from "@astrojs/react";
 
 // https://astro.build/config
@@ -10,7 +11,10 @@ export default defineConfig({
   site: 'https://videoadsbenchmark.netlify.app/', 
   base: '/', 
   output: 'static', //
-  integrations: [tailwind(), icon(), mdx(), react()],
+  integrations: [tailwind(), icon(), mdx({
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeMathjax],
+  }), react()],
   markdown: {
     shikiConfig: {
       theme: "github-light"
